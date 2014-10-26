a3875c9022ae5501559dde4e5cf8e5b605abc20d
ysaiseu <ysaiseu@google.com>
2014-10-26 08:33:44 +0800

    add a example.py as a template

---
+++ md5.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import hashlib
+
+#def 
+
+#def 
+
+#if __name__=='__main__':
+md5 = hashlib.md5()
+md5.update('how to use md5 in python hashlib?')
+print md5.hexdigest()
